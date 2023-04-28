7b492ad371fc4e01f3be2f4ee250bcd7e3544f97
winterrk <winterrk@colostate.edu>
2023-04-28 13:47:19 -0600

    Email send done.

--- force-app/main/default/lwc/addPaymentModal/addPaymentModal.js
+++ force-app/main/default/lwc/addPaymentModal/addPaymentModal.js
@@ -5,6 +5,10 @@ export default class AddPaymentModal extends LightningModal {
 
     @api defaultAmount;
 
+    updatePaymentType(e) {
+        console.log(e);
+    }
+
     handleAdd() {
         this.close({ok: true, type: this.refs.paytype.value, amount: this.refs.payamount.value, processorid: this.refs.payprocessorid.value});
     }
